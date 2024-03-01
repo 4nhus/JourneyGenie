d8d93aab028624884d0a33ed8edf5e68e0b2d92c
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-03-01 21:11:29 +1100

    Initialise Xcode project for app frontend

---
+++ frontend/JourneyGenie/JourneyGenie/JourneyGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyGenieApp.swift
+//  JourneyGenie
+//
+//  Created by Anh Nguyen on 1/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct JourneyGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
