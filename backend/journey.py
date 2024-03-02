5c32866a20dbd94acbc5984224335ea8bfc8b41f
Bill Wong <95329693+billwonggg@users.noreply.github.com>
2024-03-02 11:53:32 +1100

    Integrate with OpenAI Api (#3)
    * Add sample env
    
    add error model
    
    Apply formatter
    
    * Integrate with OpenAI api
    
    * Update get_journey function

--- backend/journey.py
+++ backend/journey.py
@@ -1,3 +1,28 @@
+from dotenv import load_dotenv
+from openai import OpenAI
 
-def get_journey(latitude:float, longitude:float, date: int) -> str:
-    pass
+
+def get_prompt(latitude: float, longitude: float, weather: str) -> str:
+    return f"""I am planning to go to Latitude {latitude}, Longitude {longitude}, the weather will be {weather}.
+    Get a 1 day itinerary including times and use real suggestions for places.
+    Return me the output in this JSON schema
+    {{ 'morning': str[], 'afternoon': str[], 'evening': str[] }}
+    """
+
+
+def get_journey(latitude: float, longitude: float, date: int) -> str:
+    load_dotenv()
+    client = OpenAI()
+
+    prompt = get_prompt(latitude, longitude, "raining")
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return completion.choices[0].message.content
+
+
+if __name__ == '__main__':
+    print(get_journey(33, 151, 1234))
